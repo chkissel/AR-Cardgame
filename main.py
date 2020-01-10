718b5903c00d54d4fb4ee9ecb35b1cbb18c84a4a
Christopher Kissel <ch.kissel@gmail.com>
2020-01-10 19:38:36 +0100

    Capture and show frames from camera

--- main.py
+++ main.py
@@ -1,10 +1,35 @@
 #!/usr/bin/env python3
-
+import cv2
 from featureDesc import FeatureDescriptor
 
 
 def game():
-    pass
+    cap = cv2.VideoCapture(0)
+    mode = 0
+    while True:
+        # Capture frame-by-frame
+        ret_cam, frame = cap.read()
+
+        # Initialize instances
+        features = FeatureDescriptor(frame)
+
+        # wait for key and switch to mode
+        ch = cv2.waitKey(1) & 0xFF
+        if ch == ord('1'):
+            mode = 1
+        elif ch == ord('q'):
+            break
+
+        if mode == 1:
+            # Extract keypoints
+            kp, des = features.extract()
+
+        # Display the resulting frame
+        cv2.imshow('frame', frame)
+
+    # When everything done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
