bfd06205c35ac95225fe16734fe2db4a9ac9d3c7
Maximilian Letter <maximilian.letter@gmail.com>
2020-02-06 17:13:49 +0100

    Try different descriptor and matching algorithms

--- featureClass.py
+++ featureClass.py
@@ -3,39 +3,49 @@ import cv2
 
 class FeatureClass:
 
-    def __init__(self, min_matches=10, max_matches=20):
+    def __init__(self, min_matches=10, max_matches=50):
         self.MINMATCHES = min_matches
         self.MAXMATCHES = max_matches
+        self.MAXDISTANCE = 50
 
         # Initiate BruteForce Matcher
-        self.bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+        # self.bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+        # self.bf = cv2.BFMatcher()
+        self.flann = cv2.DescriptorMatcher_create(cv2.DescriptorMatcher_FLANNBASED)
 
         # Initiate ORB detector
-        self.orb = cv2.ORB_create()
+        # self.orb = cv2.ORB_create()
+        self.sift = cv2.xfeatures2d.SIFT_create()
 
     def extract(self, img):
         # find the keypoints with ORB
-        kp = self.orb.detect(img, None)
-
-        # compute the descriptors with ORB
-        kp, des = self.orb.compute(img, kp)
+        # kp, des = self.orb.detectAndCompute(img, None)
+        kp, des = self.sift.detectAndCompute(img, None)
 
         return kp, des
 
     def match(self, img_des, card_des):
         # Match frame descriptors with card descriptors
-        matches = self.bf.match(img_des, card_des)
+        # matches = self.bf.match(img_des, card_des)
+        # matches = self.flann.match(img_des, card_des)
+        #
+        # # Sort them in the order of their distance
+        # matches = sorted(matches, key=lambda x: x.distance)
+        #
+        # # Limit amount of found matches, should be around 50
+        # matches = matches[:self.MAXMATCHES]
+        #
+        # # Apply ratio test
+        # good = []
+        # for m in matches:
+        #     if m.distance < self.MAXDISTANCE:
+        #         good.append(m)
 
-        # Sort them in the order of their distance
-        matches = sorted(matches, key=lambda x: x.distance)
-
-        # Limit amount of found matches, should be around 50
-        matches = matches[:self.MAXMATCHES]
-
-        # Apply ratio test
+        # matches = self.bf.knnMatch(img_des, card_des, k=2)
+        matches = self.flann.knnMatch(img_des, card_des, k=2)
         good = []
-        for m in matches:
-            if m.distance < 75:
+        for m,n in matches:
+            if m.distance < 0.75*n.distance:
                 good.append(m)
 
         return good
