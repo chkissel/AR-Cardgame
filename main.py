5de9d92921be4b93c54379bfd84c53c93148f846
Maximilian Letter <maximilian.letter@gmail.com>
2020-02-06 17:16:03 +0100

    Add new card

--- main.py
+++ main.py
@@ -12,17 +12,17 @@ from card import Card
 def game(fps, video):
     if (fps):
         start_time = time.time()
-        interval = 3 # displays the frame rate every x second
+        interval = 1 # displays the frame rate every x second
         counter = 0
 
     # Initialize instances
-    features = FeatureClass(min_matches = 20, max_matches = 75)
+    features = FeatureClass(min_matches = 20, max_matches = 50)
     geometry = GeometryClass()
 
     card_1 = Card('card_1', 50, (27, 27, 211), features)
-    card_2 = Card('card_2', 25, (211, 27, 27), features)
-    # cards = [card_1, card_2]
-    cards = [card_1]
+    card_2 = Card('card_5', 50, (211, 27, 27), features)
+    cards = [card_1, card_2]
+    # cards = [card_1]
 
     cap = cv2.VideoCapture(0)
     if (video):
